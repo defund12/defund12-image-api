ffb0febba19a6c9f6d054e8c42252f8700553935
rpalakkal <roshan.palakkal@gmail.com>
2020-06-22 14:13:13 -0400

    changed api input format

--- previews.js
+++ previews.js
@@ -14,71 +14,28 @@ Visit http://localhost:5000`);
 
 const previewTemplate = fs.readFileSync(path.resolve(__dirname, "previewImage.html"), "utf8");
 
-//deprecated - remove after succesful usage of other api route
-app.get("/api/preview", async (req, res) => {
-  if (!req.query.city) {
-    res.status(400).send("You need to specify city");
-  }
+let defaultTemplateStrings = {
+  'header':"Defund12.org",
+  'subheader':"", 
+  'subtitle1':"Send pre-scripted emails and physical letters to government officials and council members.", 
+  'subtitle2':"Demand they reallocate egregious police budgets towards education, social services, and dismantling racial inequality."
+}
 
-  if (!req.query.state) {
-    res.status(400).send("You need to specify state");
-  }
-
-  const { city, state } = req.query;
-
-  const text = `Send pre-scripted emails to ${city} government officials.`
-
-  // Support linew breaks
-  const formattedCity = city.replace(/(?:\r\n|\r|\n)/g, "<br>");
+app.get("/api/preview*", async (req, res) => {
+  let template = {...defaultTemplateStrings}
+  Object.keys(template).forEach((field)=>{
+    if(req.query[field]){
+      template[field]=req.query[field]
+    }
+  })
 
   const image = await getImage({
     // output: "./image.png",
 
-    content: {city: formattedCity, state, text},
+    content: template,
     html : previewTemplate,
   });
-
 
   res.writeHead(200, { "Content-Type": "image/png" });
   res.end(image, "binary");
 });
-
-app.get("/api/preview/:type", async (req, res) => {
-  if (!req.query.city) {
-    res.status(400).send("You need to specify city");
-  }
-
-  if (!req.query.state) {
-    res.status(400).send("You need to specify state");
-  }
-
-  const { city, state } = req.query;
-
-  
-  let text;
-  switch(req.params.type) {
-    case "letter": 
-      text = `Order physical letters to be printed and mailed to ${city} government officials.`
-      break;
-    case "email":
-      text = `Send pre-scripted emails to ${city} government officials.`
-      break;
-    default:
-      text = "Send pre-scripted emails to government officials"
-  }
-
-  // Support linew breaks
-  const formattedCity = city.replace(/(?:\r\n|\r|\n)/g, "<br>");
-
-  const image = await getImage({
-    // output: "./image.png",
-
-    content: {city: formattedCity, state, text},
-    html : previewTemplate,
-  });
-
-
-  res.writeHead(200, { "Content-Type": "image/png" });
-  res.end(image, "binary");
-});
-
